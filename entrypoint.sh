af12925442600e46d36158b05073c27d38109466
Cornelius Koller <55432690+ckmuun@users.noreply.github.com>
2021-03-12 07:35:51 +0100

    add change dir option and a few debug echos

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ EXTRACT_TAG_FROM_GIT_REF=$4
 DOCKERFILE=$5
 BUILD_CONTEXT=$6
 PULL_IMAGE=$7
+BUILD_FOLDER=$8
 
 if [ $EXTRACT_TAG_FROM_GIT_REF == "true" ]; then
   DOCKER_IMAGE_TAG=$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g")
@@ -15,11 +16,16 @@ fi
 DOCKER_IMAGE_NAME=$(echo docker.pkg.github.com/${GITHUB_REPOSITORY}/${DOCKER_IMAGE_NAME} | tr '[:upper:]' '[:lower:]')
 DOCKER_IMAGE_NAME_WITH_TAG=$(echo ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} | tr '[:upper:]' '[:lower:]')
 
+echo "login to docker.pkg.github.com"
 docker login -u publisher -p ${DOCKER_TOKEN} docker.pkg.github.com
 
 if [ $PULL_IMAGE == "true" ]; then
+  echo "pulling docker image"
   docker pull $DOCKER_IMAGE_NAME_WITH_TAG || docker pull $DOCKER_IMAGE_NAME || 1
 fi
 
+echo "change dir to build folder $BUILD_FOLDER"
+cd $BUILD_FOLDER
+ls
 docker build -t $DOCKER_IMAGE_NAME_WITH_TAG -f $DOCKERFILE $BUILD_CONTEXT
 docker push $DOCKER_IMAGE_NAME_WITH_TAG
